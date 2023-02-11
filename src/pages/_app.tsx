e9fc5dcbcfdd46e90dabc293c3a4f7c860ef334e
Juno <dghsum@gmail.com>
2023-02-11 19:06:00 +0900

    [!BreakingChange] Init 컴포넌트 추가

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+//Styles
+import '../global.css'
+
+interface ReactComponent {
+    Component: React.FC
+    pageProps: any
+}
+
+const MyApp: React.FC<ReactComponent> = function({ Component, pageProps }) {
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
