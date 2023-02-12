e7cb82db3d810b27a8a3723c48e876d013811f8b
Juno <dghsum@gmail.com>
2023-02-12 19:39:46 +0900

    [Add] Header 컴포넌트 추가
    custom style 생성 with tailwind (practice)

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = function(){
+  
+    return (
+      <header className='header'>
+        This is Header
+      </header>
+    )
+  }
+
+export default Header
