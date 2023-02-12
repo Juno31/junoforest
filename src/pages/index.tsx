7831efcf64aac1e56f138fb4da1551fba8a64480
Juno <dghsum@gmail.com>
2023-02-12 19:41:14 +0900

    [Design] home에 tailwind css 적용 (practice)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,28 @@ import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
+//Components
+import Header from 'components/Header'
+
 function Home() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <div>
-        <p className='text-blue-600'>
+      <Header />
+      <div className='flex flex-col min-h-full bg-slate-800'>
+        <p className='p-10 basis-1'>
           this is p tag
         </p>
-        <ul>
-          <li>
+        <ul className='flex-1 p-10 flex flex-col justify-around items-center'>
+          <li className='basis-1/3'>
             <Link href={'/forest'}>forest</Link>
           </li>
-          <li>
+          <li className='basis-1/3'>
             <Link href={'/awakecorp'}>awakecorp</Link>
           </li>
-          <li>
+          <li className='basis-1/3'>
             <Link href={'/hanyang'}>hanyang</Link>
           </li>
         </ul>
